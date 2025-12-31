0b814dd339c1f7fda7d5e5562e51e9dcf4a6b8ae
Brendan Murty <brendan@murty.au>
2026-01-01 10:10:47 +1100

    Fix setup script for CI

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9875b5458ad0e001444842d50739984e0b86c3f5ae48b4d482fcf211bbf93a59
-size 2243
+oid sha256:3a3c70f06ebc988eddf49c62dd52792f2bf650d62ddaf34807c01e04a8fd5431
+size 2215
