e049c246f55d84ee5090e46b5af13f309d80b783
bmurty <bcm@murty.au>
2026-02-22 13:44:23 +1100

    Fix release notes script

--- dev/release-notes.sh
+++ dev/release-notes.sh
@@ -6,11 +6,11 @@
 # If an argument is provided when calling this script,
 # like "deno task release-notes changes.log", save the output
 # to that file, otherwise set a suitable default.
-RELEASE_FILE="${1:-"./CHANGELOG.md"}"
+RELEASE_FILE=${1:-"./CHANGELOG.md"}
 
 git fetch --tags
 
-PREV_GIT_TAG="$(git describe --tags --abbrev=0)"
+PREV_GIT_TAG=$(git describe --tags --abbrev=0)
 
 GIT_REPO_COMMIT_URL="https://github.com/bmurty/site/commit/"
 
